767a8a33c8cc3d669d71f645c0a2543bf44fca17
Tim <99256921+Vog2k@users.noreply.github.com>
2022-10-15 09:25:58 +1300

    Playing around

---
+++ InteractiveWebApp.js
@@ -0,0 +1,9 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+console.log(displayValue)
+//AC DEL ÷ 1 2 3 * 4 5 6 + 7 8 9 - . 0 %
---
+++ School.js
@@ -0,0 +1,112 @@
+
+//let or const || "var"- Globle variable Avoid using. 
+//data types: number interger, decimal, float and string, boolean:True or false | null and undefined: object - complex data type
+let name = "jack"; //'jack' single quotation
+const age = 26;
+const salary = 2000.00;
+
+// output method for Javascript
+console.log(typeof salary);
+// template string
+console.log(`Hello, ${name}`);
+// backtake ``
+
+// logic control - if
+const isAdult = true;
+
+// null and undefined
+console.log(`The data type of null is ${typeof null}`);
+console.log(`The data type of undefined is ${typeof undefined}`);
+// null means we defined a varible and assign a value 'null' to it
+
+// While 'undefined' means we define a variable, but never assign a value to it
+
+let phone; //undefined
+let address = null;// Dont know the value to this varibale.
+
+const fruits = ['Smith', 12, false];
+// add new data
+fruits.push("apple");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+console.log(fruits[2]);
+
+// destructing method
+let [smith, _, bool] = fruits;
+console.log(smith, bool)
+// use "_" to repersent the data you dont want
+
+// object
+const student = { 
+firstName: "Jack",
+lastName: "Bruh",
+grade: 0.0
+};
+// Person is an entity and car is an entity
+// Array notaion
+student["IT5039 Client-Side Dev"] = 95.55;
+
+// dot notation
+student.postcode = 6014;
+console.log(student);
+
+
+//delete student.lastName;
+
+const {lastName, postcode} = student;
+console.log(lastName, postcode);
+
+//if, while, for
+//Student is an object, Fruits is an array
+
+if(bool){
+    console.log("You are able to vote now");
+}else{
+    console.log("Sike go home")
+}
+
+//while and for are used for completing the repeat tasks
+// to get esum of 1 through 50
+
+
+let count = 0;
+let sum = 0;
+
+while(count <=10){
+    sum += count;
+    count = count + 1;
+}
+ console.log(sum);
+
+
+const fruit = ['Apple', "orange", "cherry"];
+
+fruit.forEach(getEachFruit); 
+//Call back function
+//forEach is designed for retriving each data insdie an array
+
+        function getEachFruit(data){
+        console.log(data); //Function body
+        }
+
+fruit.forEach((data) => {
+    console.log(data);
+});
+
+//arrow functions
+let hey = (name) => {
+    console.log(`Hey ${name}`);
+};
+
+hey("Tim");
+//for(){}
+
+
+//for loop is also used for getting each data in either an array or object
+
+
+
+
